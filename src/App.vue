f999a61d20b80902c92e932ae2399064fea966e3
Civan <civan.erbay@web.de>
2021-05-09 20:56:16 +0200

    Add first navigation draft

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,32 @@
 <template>
   <div id="app">
-    <div class="nav">
-      <router-link to="/">Landing</router-link>|<router-link to="/about"
-        >About</router-link
-      >
+    <div class="nav" :class="{ 'nav--active': navActive }">
+      <a class="nudes">nudes</a>
+      <a class="portraits">portraits</a>
+      <a class="space">space</a>
+      <a class="about">about</a>
     </div>
-    <router-view />
+    <Landing
+      @clickedHeadline="navActive = true"
+      @clickedBackdrop="navActive = false"
+    ></Landing>
   </div>
 </template>
+
+<script>
+import Landing from "./views/Landing.vue";
+
+export default {
+  name: "App",
+  components: { Landing },
+  data() {
+    return {
+      navActive: false,
+    };
+  },
+  methods: {},
+};
+</script>
 
 <style lang="scss">
 @import url("https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&display=swap");
@@ -17,30 +36,92 @@
   --textColor: #374961;
   --linkActiveColor: #41b783;
 }
-
+body {
+  padding: 0;
+  margin: 0;
+}
 #app {
   font-family: var(--font);
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: var(--textColor);
-
-  .logo {
-    width: 20%;
-  }
 }
 
 .nav {
-  padding: 30px 0 100px 0;
+  transform: translateY(-700px);
+  position: absolute;
+  transition: ease-out 0.9s;
+  width: 100%;
+  height: 450px;
+  z-index: 2;
+  background-color: black;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+
+  &--active {
+    transform: translateY(0);
+  }
 
   a {
-    font-weight: 500;
-    color: var(--textColor);
-    margin: 0 5px;
+    font-weight: bold;
+    font-size: 36px;
+    color: white;
+    margin: 0 35px;
+    text-decoration: none;
+    cursor: pointer;
+
+    &:first-child {
+      margin-left: 150px;
+    }
+
+    &.about {
+      right: 30px;
+      top: 50px;
+      font-size: 30px;
+      position: absolute;
+    }
   }
 
-  a.router-link-exact-active {
-    color: var(--linkActiveColor);
+  .nudes:after {
+    content: "";
+    width: 80px;
+    height: 4px;
+    background-color: white;
+    display: block;
+    position: relative;
+    bottom: -40px;
   }
+  .portraits:after {
+    content: "";
+    width: 70px;
+    height: 4px;
+    background-color: white;
+    display: block;
+    position: relative;
+    bottom: -20px;
+  }
+  .space:after {
+    content: "";
+    width: 50px;
+    height: 4px;
+    background-color: white;
+    display: block;
+    position: relative;
+    bottom: -5px;
+  }
+
+  .about:before {
+    left: -13px;
+    content: "";
+    width: 4px;
+    height: 30px;
+    background-color: white;
+    display: block;
+    position: absolute;
+}
+
+
 }
 </style>
--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -1,33 +1,79 @@
 <template>
   <div id="landing" class="landing-wrapper">
-    <h1 class="headline-link">hannafreres</h1>
+    <h1 @click="onClickHeadline()" class="headline-link">hannafreres</h1>
+    <transition name="fade">
+      <div
+        v-if="showBackdrop"
+        @click="onClickBackdrop()"
+        class="backdrop"
+        :class="{'backdrop--active': showBackdrop}"
+      ></div>
+    </transition>
   </div>
 </template>
 
 <script>
 export default {
-  
-}
+  name: "Landing",
+  data() {
+    return {
+      showBackdrop: false,
+    };
+  },
+  methods: {
+    onClickHeadline: function (e) {
+      this.$emit("clickedHeadline");
+      this.showBackdrop = true;
+    },
+    onClickBackdrop: function (e) {
+      this.$emit("clickedBackdrop");
+      this.showBackdrop = false;
+    },
+  },
+};
 </script>
 
 <style lang="scss">
-  .landing-wrapper {
-    
-      width: 100vw;
-      height: 100vh;
-      background: url('../assets/img1.jpeg') no-repeat 0 0;
-      background-position: center;
-      background-size: contain;
-      position: relative;
-    
+.landing-wrapper {
+  padding: 0;
+  margin: 0;
+  width: 100vw;
+  height: 100vh;
+  background: url("../assets/img1.jpeg") no-repeat 0 0;
+  background-position: center;
+  background-size: cover;
+  position: relative;
 
-    .headline-link {
-        position: absolute;
-        font-weight: 300;
-        font-size: 4rem;
-        color: white;
-        top: 150px;
-        left: 100px;
-    }
+  .headline-link {
+    position: absolute;
+    font-weight: 300;
+    font-size: 75px;
+    color: white;
+    top: 30px;
+    left: 75px;
+    cursor: pointer;
   }
+
+  .backdrop {
+    width: 100vw;
+    height: 100vh;
+    z-index: 1;
+    background-color: rgba($color: #000000, $alpha: 0.8);
+    transition: opacity 0.5s ease-in;
+
+   /*  &--active {
+      opacity: 0.8;
+    } */
+  }
+
+
+  .fade-enter-active,
+  .fade-leave-active {
+    transition: opacity 0.5s;
+  }
+  .fade-enter,
+  .fade-leave-to {
+    opacity: 0.5;
+  } 
+}
 </style>
