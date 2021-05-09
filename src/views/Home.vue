558ee084217f3ba14dd1aaff657358011fdd89c2
Civan <civan.erbay@web.de>
2021-05-09 11:10:07 +0200

    Add basic setup

--- src/views/Home.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div id="home">
-    <img class="logo" src="../assets/logo.png" alt="logo" />
-
-    <h1>hannafreres</h1>
-  </div>
-</template>
---
+++ src/views/Landing.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="landing" class="landing-wrapper">
+    <h1 class="headline-link">hannafreres</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="scss">
+  .landing-wrapper {
+    
+      width: 100vw;
+      height: 100vh;
+      background: url('../assets/img1.jpeg') no-repeat 0 0;
+      background-position: center;
+      background-size: contain;
+      position: relative;
+    
+
+    .headline-link {
+        position: absolute;
+        font-weight: 300;
+        font-size: 4rem;
+        color: white;
+        top: 150px;
+        left: 100px;
+    }
+  }
+</style>
